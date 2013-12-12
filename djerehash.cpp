da210c0b32bd87207be794ae86a29ae809251a86
Rex.Djere <rdjere gmail>
2013-12-12 17:28:16 -0500

    Signed-off-by: Rex.Djere <rdjere gmail>

--- djerehash.cpp
+++ djerehash.cpp
@@ -3,7 +3,7 @@
 **********************************************************************
 * Author: Rex Djere                                                  *
 * Contact: rdjere gmail                                              *
-* Revision: 0.16, 12/12/2013 (see revisions.txt and peerReview.txt)  *
+* Revision: 0.17, 12/12/2013 (see revisions.txt and peerReview.txt)  *
 * License: GNU GPL v.3 (see license.txt)                             *
 * Copyright: Rex Djere, 12/06/2013                                   *
 *********************************************************************/
@@ -19,9 +19,8 @@ using namespace std;
 
 int main ()
 {
-    unsigned int hashBins[256] = {}; // stores user input in bins (bin 1 => 0-256, bin 2 => 257-513 etc. etc.)
-    unsigned int binSum = 0;
-    char input[256]={};
+    unsigned int ipSum = 0; // stores value proportional to sum of user input
+    char input[256]={}; // stores the user input
     const unsigned char charSet[62] = {'0','1','2','3','4','5','6','7','8','9',
     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
     'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
@@ -37,26 +36,22 @@ int main ()
             for(int j=0; j< 256; j++)
             {
                 // hash function is exponential to provide maximum randomness of hash output at moderate cost.
-                binSum = binSum + (input[j]+1)*pow(j+1,2);
-                binSum = binSum * pow(j+1,1.5);
+                ipSum = ipSum + (input[j]+1)*pow(j+1,2);
+                ipSum = ipSum * pow(j+1,1.5);
             }
 
             if(i%7==0)
             {
-                cout << charSet[binSum%62]; // 36 character hash output
+                cout << charSet[ipSum%62]; // 36 character hash output
             }
         }
         cout << endl << "Please enter 1 to hash another string, or 0 to quit:" << " ";
         cin >> Pcontinue; // to do: add detection for erroneous input.
         cin.ignore(Pcontinue); // clear cin to avoid error in cin getline around line 35
 
-        // reset all arrays to zero
-        for(int k = 0; k < 256; k++)
-        {
-            input[k]=0;
-            hashBins[k]=0;
-        }
-            binSum = 0;
+        // reset input array to zero
+        for(int k = 0; k < 256; k++) input[k]=0;
+        ipSum = 0;
     }
     return 0;
 }
